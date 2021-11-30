b1f5fc35bf1734c7428bb6ed4ca61a76c84c8ae3
AashuPari <smuskaansha@gmail.com>
2021-11-30 12:05:15 +0530

    first commit

--- main.py
+++ main.py
@@ -20,28 +20,22 @@ def upload_video():
 
 
 # Code for Project 265 Grayscale start here
-   
+   source = cv2.VideoCapture('static/'+ filename)
+   frame_width = int(source.get(3))
+   frame_hight = int(source.get(4))
+   size = (frame_hight, frame_width)
 
+   result = cv2.VideoWriter('static/' + 'black_and_white.mp4', cv2.VideoWriter_fource(*'mp4'), 30, size, 0)
 
+   try:
+   	while True:
+   		status, frame_image = source.read()
+   		gray = cv2.cvtColor(frame_image, cv2.COLOR_BGR2GRAY)
+   		result.write(gray)
+   		video_file = 'black_and_white.mp4'
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-   
-            
+   	except:
+   		print('Completed reading all the Frames from the Video')      
 # Code for Project 265 end here
 
     return render_template('upload.html', filename=filename)
@@ -49,12 +43,10 @@ def upload_video():
 
 
 # Code for Project 265 download funcion starts here
-
-
-
-
-
-
+@app.route('/download')
+def download_file():
+	converted_video_path = "static/black_and_white.mp4"
+	return send_file(converted_video_path, as_attachment=True)
 
 # Code for Project 265 download funcion ends here
 
